c22bd694d80778bb9367bdd0568e32f451d041fa
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2023-11-08 22:58:40 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const loginForm = document.querySelector(".login-form");
 
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const { email, password } = this.elements;
+
+    if (email.value === "" || password.value === "") {
+      alert("Please fill in all the fields.");
+      return;
+    }
+
+    const formData = {
+      email: email.value,
+      password: password.value,
+    };
+
+    console.log(formData);
+
+    this.reset();
+  });
+});
