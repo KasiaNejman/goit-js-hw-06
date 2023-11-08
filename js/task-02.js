50846480c83696594a8256aac79c2f65d6920bfc
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2023-11-08 22:58:23 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const list = document.querySelector("#ingredients");
+
+for (const ingridient of ingredients) {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingridient;
+  listItem.classList.add("item");
+  list.append(listItem);
+}
